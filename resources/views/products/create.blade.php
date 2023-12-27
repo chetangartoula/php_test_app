a1d37d4dcd42216a9e15cb549eefb13c24a735f3
Chetan Gartoula <63311697+chetangartoula@users.noreply.github.com>
2023-12-27 21:49:24 +0545

    fixing layout and for the template

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -1,81 +1,41 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layout.product_layout')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Laravel CRUD</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
-    <script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
-</head>
+@section('main')
+    <div class="col-md-8">
+        <form action="{{ route('store.product') }}" method="POST" enctype="multipart/form-data">
+            @csrf
+            <div class="card mt-3 p-4">
+                <div class="form-group">
+                    <label for="name">Product Name:</label>
+                    <input type="text" class="form-control" id="name" placeholder="Enter Product Name"
+                        value="{{ old('name') }}" name="name">
+                    <div class="valid-feedback">Valid.</div>
+                    <div class="invalid-feedback">Please fill out this field.</div>
+                    @if ($errors->has('name'))
+                        <div class="text-danger">{{ $errors->first('name') }}</div>
+                    @endif
+                </div>
+                <div class="form-group">
+                    <label for="description">description:</label>
+                    <input type="text" class="form-control" id="description" placeholder="Enter description"
+                        value="{{ old('description') }}" name="description">
+                    <div class="valid-feedback">Valid.</div>
+                    <div class="invalid-feedback">Please fill out this field.</div>
+                    @if ($errors->has('description'))
+                        <div class="text-danger">{{ $errors->first('description') }}</div>
+                    @endif
+                </div>
+                <div class="form-group">
+                    <label for="product_image">Product Image:</label>
+                    <input type="file" class="form-control" id="product_image" name="product_image"
+                        value="{{ old('product_image') }}">
+                    @if ($errors->has('product_image'))
+                        <div class="text-danger">{{ $errors->first('product_image') }}</div>
+                    @endif
+                </div>
 
-<body>
-
-
-    <nav class="navbar navbar-expand-sm bg-dark">
-
-        <!-- Links -->
-        <ul class="navbar-nav">
-            <li class="nav-item">
-                <a class="nav-link text-light" href="#">Products</a>
-            </li>
-
-        </ul>
-
-    </nav>
-
-
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-
-                @if ($message = Session::get('success'))
-                    <div class="alert alert-success alert-block mt-1">
-                       <strong>{{ $message }}</strong> 
-                    </div>
-                @endif
-                <form action="{{ route('store.product') }}" method="POST" enctype="multipart/form-data">
-                    @csrf
-                    <div class="card mt-3 p-4">
-                        <div class="form-group">
-                            <label for="name">Product Name:</label>
-                            <input type="text" class="form-control" id="name" placeholder="Enter Product Name"
-                                value="{{ old('name') }}" name="name">
-                            <div class="valid-feedback">Valid.</div>
-                            <div class="invalid-feedback">Please fill out this field.</div>
-                            @if ($errors->has('name'))
-                                <div class="text-danger">{{ $errors->first('name') }}</div>
-                            @endif
-                        </div>
-                        <div class="form-group">
-                            <label for="description">description:</label>
-                            <input type="text" class="form-control" id="description" placeholder="Enter description"
-                                value="{{ old('description') }}" name="description">
-                            <div class="valid-feedback">Valid.</div>
-                            <div class="invalid-feedback">Please fill out this field.</div>
-                            @if ($errors->has('description'))
-                                <div class="text-danger">{{ $errors->first('description') }}</div>
-                            @endif
-                        </div>
-                        <div class="form-group">
-                            <label for="product_image">Product Image:</label>
-                            <input type="file" class="form-control" id="product_image" name="product_image"
-                                value="{{ old('product_image') }}">
-                            @if ($errors->has('product_image'))
-                                <div class="text-danger">{{ $errors->first('product_image') }}</div>
-                            @endif
-                        </div>
-
-                        <button type="submit" class="btn btn-primary">Submit</button>
-                    </div>
-                </form>
+                <button type="submit" class="btn btn-primary">Submit</button>
             </div>
-        </div>
+        </form>
     </div>
-
-</body>
-
-</html>
+@endsection
--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,97 +1,64 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layout.product_layout')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Laravel CRUD</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
-    <script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
-</head>
-
-<body>
+@section('main')
+    <div class="col-md-6"> <a href="{{ route('create.product') }}" class="btn btn-primary mt-4 text-align-left">
+            Create Product
+        </a></div>
 
 
-    <nav class="navbar navbar-expand-sm bg-dark">
+    <div class="col-md-8 mt-3 justify-content-center">
 
-        <!-- Links -->
-        <ul class="navbar-nav">
-            <li class="nav-item">
-                <a class="nav-link text-light" href="#">Products</a>
-            </li>
-
-        </ul>
-
-    </nav>
-    <div class="container">
-        <div class="row">
-            <a href="{{ route('create.product') }}" class="btn btn-primary mt-4 text-align-right">
-                Create Product
-            </a>
-        </div>
-        <div class="row mt-3">
-
-            <div class="col-md-8 justify-content-center">
-
-                <table class="table table-striped">
-                    <thead>
-                        <tr>
-                            <th>sno</th>
-                            <th>Name</th>
-                            <th>Description</th>
-                            <th>Image</th>
-                            <th>Action</th>
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th>sno</th>
+                    <th>Name</th>
+                    <th>Description</th>
+                    <th>Image</th>
+                    <th>Action</th>
 
 
 
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($products as $item)
-                      
-                            <tr>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($products as $item)
+                    <tr>
 
 
 
-                                <td>{{ $loop->index + 1 }}</td>
-                                <td>{{ $item->name }}</td>
-                                <td>{{ $item->description }}</td>
-                                <td>
-                                    <img src="/products/{{ $item->image }}" alt="test-image" class="rounded-circle"
-                                        width="40" height="40">
-                                </td>
+                        <td>{{ $loop->index + 1 }}</td>
+                        <td>{{ $item->name }}</td>
+                        <td>{{ $item->description }}</td>
+                        <td>
+                            <img src="/products/{{ $item->image }}" alt="test-image" class="rounded-circle" width="40"
+                                height="40">
+                        </td>
 
-                                <td>
-                                    <a href="products/{{ $item->id }}/edit" class="btn btn-dark btn-sm"> Edit
-                                        Product</a>
-
-
-                                    
-                                  
-                                    <form action="{{route('store.product.delete',$item->id)}}"  class="d-inline" method="POST">
-                                    @csrf
-                                    @method('delete')
-                                        <button type="submit" class="btn btn-danger btn-sm">Delete Product</button>
-                                    
-                                    </form>
-
-                                </td>
+                        <td>
+                            <a href="products/{{ $item->id }}/edit" class="btn btn-dark btn-sm"> Edit
+                                Product</a>
 
 
-                            </tr>
-                        @endforeach
 
 
-                    </tbody>
-                </table>
+                            <form action="{{ route('store.product.delete', $item->id) }}" class="d-inline" method="POST">
+                                @csrf
+                                @method('delete')
+                                <button type="submit" class="btn btn-danger btn-sm">Delete Product</button>
 
-            </div>
-        </div>
+                            </form>
+
+                        </td>
+
+
+                    </tr>
+                @endforeach
+
+
+            </tbody>
+        </table>
 
     </div>
-</body>
-
-</html>
+    </div>
+@endsection
